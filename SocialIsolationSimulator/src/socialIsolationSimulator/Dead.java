102822eba2c2589b0e65e210b52c70a3059781f9
UrviAryamane19 <aryamane.u@northeastern.edu>
2021-04-06 20:37:44 -0400

    Committing to Dead.java & Recovered.java

--- SocialIsolationSimulator/src/socialIsolationSimulator/Dead.java
+++ SocialIsolationSimulator/src/socialIsolationSimulator/Dead.java
@@ -1,5 +1,15 @@
 package socialIsolationSimulator;
 
-public class Dead {
+import repast.simphony.space.continuous.ContinuousSpace;
+import repast.simphony.space.grid.Grid;
 
+public class Dead {
+	private ContinuousSpace<Object> space;
+	private Grid<Object> grid;
+
+
+	public Dead(ContinuousSpace<Object> space, Grid<Object> grid) {
+		this.space = space;
+		this.grid = grid;		
+	}
 }
