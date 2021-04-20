98ebf8157d80b440e6c1fcc2b0ecf05f140b9c45
vishakha.vi <vinayak.v@northeastern.edu>
2021-04-20 00:08:05 -0400

    Added config.ini file

--- PSA_Final_Project/src/main/src/java/INFO6205/MainJFrame.java
+++ PSA_Final_Project/src/main/src/java/INFO6205/MainJFrame.java
@@ -1,11 +1,15 @@
 
 package INFO6205;
 
+import org.ini4j.*;
+
 import javax.swing.*;
+import java.io.File;
+import java.io.IOException;
 import java.util.Timer;
 
 public class MainJFrame {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		new MainJFrameHelper();
 		while(!SimulationValues.start12)
 			try {
@@ -15,9 +19,15 @@ public class MainJFrame {
 			}
 		    new SimulationValues();
 			new DisplaySpreadHelper();
-		DisplaySpreadHelper.displaySpreadHelper.spreadCanavas(false, 10,5,1);
-		DisplaySpreadHelper.displaySpreadHelper.spreadCanavas(true,50 , 5,1 );
-		int peopleCount=1000;
+		Wini ini = new Wini(new File("..\\PSA_Final_Project\\src\\main\\config.ini"));
+		int placeSize= ini.get("canvas","placeSize",int.class);
+		int varSize = ini.get("canvas","varSize",int.class);
+
+
+		DisplaySpreadHelper.displaySpreadHelper.spreadCanavas(false, 10,placeSize,varSize);
+		DisplaySpreadHelper.displaySpreadHelper.spreadCanavas(true,50 , placeSize,varSize);
+		int peopleCount= ini.get("canvas","peopleCount",int.class);
+
 		DisplaySpreadHelper.displaySpreadHelper.populatePeople(peopleCount);
 		DisplaySpreadHelper.displaySpreadHelper.initiateDisease();
 
